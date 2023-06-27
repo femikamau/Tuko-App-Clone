53789ea1f91b513a52e761cfeac3ecd9236391f9
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-27 17:57:31 +0300

    config(project): update `settings.py` file
    Add `posts` app to installed apps

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps.users.apps.UsersConfig",
+    "apps.posts.apps.PostsConfig",
 ]
 
 AUTH_USER_MODEL = "users.User"
