7ac748497e8ea14db88e77be00d5f85889cbc231
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-27 19:38:06 +0300

    feat(project): create apis endpoint route

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+    path(route="admin/", view=admin.site.urls),
+    path(route="api/", view=include("apis.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
