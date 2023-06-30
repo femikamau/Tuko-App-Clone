be56e415fce17a0d4a884485da94706f1fbca4ce
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-30 14:06:25 +0300

    feat(apps): add register user functionality

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import User
 
@@ -20,3 +22,49 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name",
             "posts",
         )
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[
+            UniqueValidator(queryset=User.objects.all()),
+        ],
+    )
+    password = serializers.CharField(
+        required=True,
+        validators=[validate_password],
+        write_only=True,
+    )
+    password2 = serializers.CharField(
+        required=True,
+        write_only=True,
+        label="Password Confirmation",
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password2",
+        )
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            password=validated_data["password"],
+        )
+
+        return user
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,18 @@
-from rest_framework.permissions import IsAdminUser
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 
 class UserViewSet(ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAdminUser,)
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
