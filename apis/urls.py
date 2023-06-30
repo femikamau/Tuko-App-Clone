8c97d42884ab36ab987ec1b18ac4ec23c704e8b8
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-30 14:11:09 +0300

    feat(project): add new routes
    Add routes for the news feed and user registration views

--- apis/urls.py
+++ apis/urls.py
@@ -2,15 +2,17 @@ from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
-from apps.posts.views import PostViewSet
-from apps.users.views import UserViewSet
+from apps.posts.views import FeedViewSet, PostViewSet
+from apps.users.views import RegisterView, UserViewSet
 
 router = DefaultRouter()
 
 router.register(prefix=r"users", viewset=UserViewSet, basename="user")
+router.register(prefix=r"feed", viewset=FeedViewSet, basename="feed")
 router.register(prefix=r"posts", viewset=PostViewSet, basename="post")
 
 urlpatterns = [
+    path(route="register/", view=RegisterView.as_view(), name="register"),
     path(route="login/", view=obtain_auth_token),
-    path("rest-auth/", include("rest_framework.urls")),
+    path(route="rest-auth/", view=include("rest_framework.urls")),
 ] + router.urls
