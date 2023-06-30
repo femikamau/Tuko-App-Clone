e743de3daa8779753056dc43065d65e0af0f447e
Femi Kamau <83460313+femikamau@users.noreply.github.com>
2023-06-30 14:07:26 +0300

    feat(apps): implement news feed viewset

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -3,18 +3,42 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.ModelSerializer):
+class ReadPostSerializer(serializers.ModelSerializer):
     """
-    Post Serializer.
+    Read Post Serializer
+
+    To be applied on views that have read-only functionality
     """
 
     url = serializers.HyperlinkedIdentityField(view_name="post-detail")
 
-    author = serializers.HyperlinkedRelatedField(
-        read_only=True, view_name="user-detail"
+    author = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field="full_name"
     )
 
     class Meta:
         model = Post
-        fields = ("id", "url", "headline", "content", "image", "status", "author")
-        read_only_fields = ("id", "author")
+        fields = (
+            "url",
+            "headline",
+            "content",
+            "image",
+            "author",
+            "published_at",
+        )
+        read_only_fields = ("published_at",)
+
+
+class WritePostSerializer(ReadPostSerializer):
+    """
+    Write Post Serializer
+
+    Extends the `ReadPostSerializer`.
+    To be applied on views that have `Create`, `Read`, `Update`, and `Delete`
+    functionality
+
+    Adds `status` to the fields
+    """
+
+    class Meta(ReadPostSerializer.Meta):
+        fields = ReadPostSerializer.Meta.fields + ("status",)
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,55 +1,111 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
+import datetime
+
 from rest_framework import status
-from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-from .forms import PostForm
 from .models import Post
-from .serializers import PostSerializer
-
-
-class PostCreateView(CreateView):
-    form_class = PostForm
-    # template_name =
-    # success_url = "/"
-    # success_message = "Post created successfully"
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+from .serializers import ReadPostSerializer, WritePostSerializer
 
 
 class PostViewSet(ModelViewSet):
     """
-    Post ViewSet.
+    Post ViewSet
     """
 
-    queryset = Post.get_published_posts()
-    serializer_class = PostSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = WritePostSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Only authenticated users can create posts
+        if request.user.is_authenticated:
+            return super().create(request, *args, **kwargs)
+
+        # Otherwise, return a 401
+        return Response(
+            data={"detail": "Authentication credentials were not provided."},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author != request.user:
-            return Response(
-                {"detail": "You do not have permission to perform this action."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
+        # If the status is published, set the published_at field to the current time
+        if serializer.validated_data.get("status") == Post.StatusChoices.PUBLISHED:
+            serializer.instance.published_at = datetime.datetime.now()
+            serializer.instance.save()
 
-        return super().update(request, *args, **kwargs)
+        # If the status is draft, set the published_at field to None
+        elif serializer.validated_data.get("status") == Post.StatusChoices.DRAFT:
+            serializer.instance.published_at = None
+            serializer.instance.save()
+
+    def list(self, request, *args, **kwargs):
+        # If the user is authenticated, display all their posts
+        if request.user.is_authenticated:
+            self.queryset = request.user.posts.all()
+            return super().list(request, *args, **kwargs)
+
+        # Otherwise, return a 401
+        return Response(
+            data={"detail": "Authentication credentials were not provided."},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+
+    def retrieve(self, request, *args, **kwargs):
+        post = self.get_object()
+
+        # If the user is the author of the post, display the post
+        if post.author == request.user or post.status == Post.StatusChoices.PUBLISHED:
+            return super().retrieve(request, *args, **kwargs)
+
+        # Otherwise, return a 404
+        return Response(
+            data={"detail": "Not found."},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    def update(self, request, *args, **kwargs):
+        post = self.get_object()
+
+        # Only the author can update their post
+        if post.author == request.user:
+            # Check if the status is being changed to published
+            if request.data.get("status") == Post.StatusChoices.PUBLISHED:
+                # Check if the post has been published before
+                if post.published_at is None:
+                    # If it hasn't, set the published_at field to the current time
+                    post.published_at = datetime.datetime.now()
+
+            return super().update(request, *args, **kwargs)
+
+        # Otherwise, return a 403
+        return Response(
+            data={"detail": "You do not have permission to edit this post"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
 
     def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author != request.user and not request.user.is_superuser:
-            return Response(
-                {"detail": "You do not have permission to perform this action."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
+        post = self.get_object()
 
-        return super().destroy(request, *args, **kwargs)
+        # Only the author or an admin user can delete the post
+        if post.author == request.user or request.user.is_staff:
+            return super().destroy(request, *args, **kwargs)
+
+        # Otherwise, return a 403
+        return Response(
+            data={"detail": "You do not have permission to delete this post"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+
+class FeedViewSet(ListModelMixin, GenericViewSet):
+    """
+    Feed ViewSet
+    """
+
+    queryset = Post.get_published_posts()
+    serializer_class = ReadPostSerializer
+    permission_classes = (AllowAny,)
